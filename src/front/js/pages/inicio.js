d5234b2c5d49192a79af1604222b0836b6cd5910
Almudena De La Rosa <almudenaxico@gmail.com>
2021-10-04 18:50:14 +0000

    inicio

--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import "../../styles/home.scss";
+import Button from "react-bootstrap/Button";
 
 export const Inicio = () => {
 	const { store, actions } = useContext(Context);
@@ -9,7 +10,11 @@ export const Inicio = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>INICIO</h1>
-			<Link to="/login"><button>Comenzamos</button></Link>
+			<Link to="/login">
+				<button className="mt-5" size="lg">
+					Comenzamos
+				</button>
+			</Link>
 		</div>
 	);
 };
