7695382681610f3497223eefe89deecc5f98ffb8
EliezerRguez <eliezer.rguez93@gmail.com>
2021-10-04 18:08:29 +0000

    info page created

--- src/front/js/pages/info.js
+++ src/front/js/pages/info.js
@@ -3,16 +3,12 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import "../../styles/home.scss";
 
-export const Home = () => {
+export const Info = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>HOME</h1>
-			<Link to="/profile">Profile</Link>
-			<Link to="/info">Info</Link>
-			<Link to="/plans">Plans</Link>
-			<Link to="/awards">Awards</Link>
+			<h1>INFORMARION</h1>
 		</div>
 	);
 };
