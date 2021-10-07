7fb80020dab8132434fb082d0b7582ffb33fd2db
EliezerRguez <eliezer.rguez93@gmail.com>
2021-10-07 18:23:42 +0000

    info and plan view

--- src/front/js/pages/info.js
+++ src/front/js/pages/info.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import "../../styles/home.scss";
 
 export const Info = () => {
@@ -9,6 +11,42 @@ export const Info = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>INFORMATION</h1>
+			<Card className="mx-4">
+				<Card.Img variant="top" src="holder.js/100px180" />
+				IMAGEN GYM
+				<Card.Body>
+					<Card.Text>
+						<div>Lorem ipsum</div>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+			<br />
+
+			<Card className="mx-4">
+				<Card.Img variant="top" src="holder.js/100px180" />
+				PROTOCOLO
+				<Card.Body>
+					<Card.Text>
+						<div>Lorem ipsum</div>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+			<br />
+
+			<Card className="mx-4">
+				<Card.Img variant="top" src="holder.js/100px180" />
+				CONTACTO
+				<Card.Body>
+					<Card.Text>
+						<Link to="/plans">
+							<Button variant="primary" type="submit">
+								LLAMA YA
+							</Button>
+						</Link>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+			<br />
 		</div>
 	);
 };
