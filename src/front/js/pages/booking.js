9ba96cb96bc407f5b03ad85b462eba74e5986ae6
Almudena De La Rosa <almudenaxico@gmail.com>
2021-10-20 07:36:27 +0000

    booking ok

--- src/front/js/pages/booking.js
+++ src/front/js/pages/booking.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-//import "../../styles/home.scss";
+import "../../styles/booking.scss";
 import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
 
 export const Booking = () => {
 	const { store, actions } = useContext(Context);
@@ -27,34 +27,28 @@ export const Booking = () => {
 				<Calendar value={date} onChange={NewDate} className="calendar" />
 			</div>
 			{showModal ? (
-				<div>
-					<Modal.Dialog>
-						<Modal.Header closeButton>
-							<Modal.Title>Available Hours</Modal.Title>
-						</Modal.Header>
-
-						<Modal.Body>
-							<p>All hours goes here!</p>
-							<Modal.Body>
-								<Button>10:00-10:45</Button>
-								<Button>10:45-11:30</Button>
-								<Button>11:30-12:15</Button>
-								<Button>12:15-13:00</Button>
-								<Button>16:00-16:45</Button>
-								<Button>16:45-17:30</Button>
-								<Button>17:30-18:15</Button>
-								<Button>18:15-19:00</Button>
-								<Button>21:00-21:45</Button>
-								<Button>21:45-22:30</Button>
-							</Modal.Body>
-						</Modal.Body>
-
-						<Modal.Footer>
-							<Button variant="secondary">Close</Button>
-							<Button variant="primary">Book</Button>
-						</Modal.Footer>
-					</Modal.Dialog>
-				</div>
+				<Card style={{ width: "100vw" }}>
+					<Card.Body>
+						<Card.Title>Horas Disponibles</Card.Title>
+						<div>
+							<Button className="boton-hours">10:00-10:45</Button>
+							<Button className="boton-hours">10:45-11:30</Button>
+							<Button className="boton-hours">11:30-12:15</Button>
+							<Button className="boton-hours">12:15-13:00</Button>
+							<Button className="boton-hours">16:00-16:45</Button>
+							<Button className="boton-hours">16:45-17:30</Button>
+							<Button className="boton-hours">17:30-18:15</Button>
+							<Button className="boton-hours">18:15-19:00</Button>
+							<Button className="boton-hours">21:00-21:45</Button>
+							<Button className="boton-hours">21:45-22:30</Button>
+						</div>
+						<div>
+							<Button variant="primary" className="book">
+								Reservar
+							</Button>
+						</div>
+					</Card.Body>
+				</Card>
 			) : null}
 		</div>
 	);
