dd3e6471c737c918bd16a9ea7adde0217e08338f
ChiaraR3 <chiara.r.3@live.it>
2021-11-03 19:25:40 +0000

    heroku

--- src/app.py
+++ src/app.py
@@ -22,8 +22,9 @@ app.config["JWT_SECRET_KEY"] = "superequipo-actimel-gimnasiohotel-apptivate"
 jwt = JWTManager(app)
 
 # database condiguration
-if os.getenv("DATABASE_URL") is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db_url = os.getenv("DATABASE_URL")
+if db_url is not None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
